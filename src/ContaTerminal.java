36ad7fbac4f445a5dd1bf2b15913505274e84b6f
Vitor Franca <vitorfranca089@gmail.com>
2025-03-07 16:28:21 -0300

    feat: criação da classe ContaTerminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,13 @@
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        //TODO: Conhecer e Importar a classe Scanner
+
+        //TODO: Exibir as mesnagens para o usuário
+
+        //TODO: Obter pelo Scanner os dados da conta
+
+        //TODO: Exibir mensagem de conta criada
+    }
+
+}
